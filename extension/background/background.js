226e4514cff2a12d4defc33516ac366832b65afa
ben philps <bp413@cam.ac.uk>
2019-06-28 17:47:53 +0100

    update manifest bugs

--- extension/background/background.js
+++ extension/background/background.js
@@ -1,1 +1,7 @@
 // background script goes here, to inject content script eventually I imagine
+
+
+// on install, eventually should setup settings, for now just write to console
+chrome.runtime.onInstalled.addListener(function (){
+    console.log("extension installed");
+});
