357e26247b3c9632a24b3c0da9e8f37af44f9759
ben philps <bp413@cam.ac.uk>
2019-06-29 10:53:06 +0100

    setup test content script
    have setup a basic content script injected into every single page that sets the bavkground to green, just to get a visual clue that my content script is infact working

--- extension/page/page.js
+++ extension/page/page.js
@@ -1,1 +1,13 @@
 // page script to be injected
+// at the moment it is injected into every single page upon the page being loaded.
+// it does nothing so far.
+
+document.addEventListener("DOMContentLoaded", function onLoad(){
+    // so here i will
+    // write to console saying page is loaded
+    // send my external message thingy
+    // see if it loads.
+    console.log("new tab has loaded - should be for every single tab");
+    // now I set every background page to green to see that my content script is working yay
+    document.body.style.backgroundColor =  "#3aa757";
+});
