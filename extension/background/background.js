df97d3feb83fb1ef62391c4b492ddef4b0fe46e6
ben philps <bp413@cam.ac.uk>
2019-06-30 10:14:20 +0100

    RESTORE
    restored old code manually,
    stupid git revert filled my code with a
    load of meta chracters

--- extension/background/background.js
+++ extension/background/background.js
@@ -1,8 +1,8 @@
 // background script goes here, to inject content script eventually I imagine
-var audioContextsList = []; // the audio contexts of current live tabs
-var gainNodesList = [];     // gain nodes for current live tabs
-var tabsList = [];          // the tabs that are currently live
 
+// testing setup for a single audio context
+var audioContext = new AudioContext();
+var gainNode = audioContext.createGain();
 
 // on install, eventually should setup settings, for now just write to console
 chrome.runtime.onInstalled.addListener(function (){
@@ -27,62 +27,13 @@ chrome.runtime.onConnect.addListener(function(port){
 });
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-// setup message listener, check for different messages and act approporiately
+// setup message listener, when tab audio requested, just output a log of the stream
 chrome.runtime.onMessage.addListener(function(request, sendResponse){
-=======
-// setup message listener, when tab audio requested, just output a log of the stream
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
->>>>>>> parent of b98dc9f... properly linked my gain node to my slider
-=======
-// setup message listener, when tab audio requested, just output a log of the stream
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
->>>>>>> parent of b98dc9f... properly linked my gain node to my slider
-=======
-// setup message listener, when tab audio requested, just output a log of the stream
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
->>>>>>> parent of b98dc9f... properly linked my gain node to my slider
     // the message listener will recieve a request message, who sent it and a callback? I think, not sure
     
     // if its my message
     if(request.action === 'fetch_audio_stream') {
         console.log("my mesage for audio fetch recieved");
-<<<<<<< HEAD
-        console.log("request: ", request);
-        console.log("send_resp: ", sendResponse);
-
-        try{
-            // now check if tab is not in 'currently being tracked tabs'
-            var currTab;
-
-            /*
-            chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
-                currTab = tabs[0];
-                console.log("currTab: ",currTab);
-            });
-            */
-
-            if(!tabsList.includes(5)){
-                
-                // capture the active tab
-                chrome.tabCapture.capture({
-			        audio : true,
-			        video : false
-		        }, function(stream) {
-			        //console.log('stream', stream);
-                    //I can attach audio stuff here
-
-                    // create an audio context
-                    var audioContext = new AudioContext();
-                    var gainNode = audioContext.createGain();
-
-                    // get a source, attach the gain node
-                    var source = audioContext.createMediaStreamSource(stream);
-                    source.connect(gainNode);
-                    gainNode.connect(audioContext.destination);
-=======
 
         chrome.tabCapture.capture({
 			audio : true,
@@ -93,9 +44,8 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
             //I can attach all my filter here...
 
             // create an audio context
-            var audioContext = new AudioContext();
-            var gainNode = audioContext.createGain();
-            //var destination = audioContext.createMediaStreamDestination();
+            //var audioContext = new AudioContext();
+            //var gainNode = audioContext.createGain();
 
             // get a source
             var source = audioContext.createMediaStreamSource(stream);
@@ -109,60 +59,21 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
             console.log("here is my audio context, source, and gain node: ", audioContext, source, gainNode);
             console.log("min and max value", gainNode.gain.minValue, gainNode.gain.maxValue);
             console.log("current value", gainNode.gain.value);
-            gainNode.gain.value *= 5;
+            gainNode.gain.value = 0;
             console.log("updated value", gainNode.gain.value);
->>>>>>> parent of b98dc9f... properly linked my gain node to my slider
             
-<<<<<<< HEAD
-<<<<<<< HEAD
-                    // now push the new nodes to the list store
-                    audioContextsList.push(audioContext);
-                    gainNodesList.push(gainNode);
-                    tabsList.push(currTab); // add tabs to tabs being monitored.
-                });
-            }
 
-        } catch(err) {
-            // in this case do nothing, since there is no audio context to capture
-            if(err.name !== 'TypeError'){
-                console.error("Not the error type I was expecting, something else went wrong here:")
-                console.error(err);
-            }
-        }
-
-        // now call the callback function:
-        sendResponse(true);
-
-<<<<<<< HEAD
+        });
     // the recieve an update gain message    
     } else if (request.action === 'update-gain'){
         console.log("value recived: " + request.slider_value);
         gainNode.gain.value = parseInt(request.slider_value)/100;
     
-    // when asked to retrive a gain node    
     } else if(request.action == 'fetch-gain-node'){
             sendResponse(gainNode);
     }
-=======
-		});
-    } 
->>>>>>> parent of b98dc9f... properly linked my gain node to my slider
-=======
-
-		});
-    } 
->>>>>>> parent of b98dc9f... properly linked my gain node to my slider
-=======
-
-		});
-    } 
->>>>>>> parent of b98dc9f... properly linked my gain node to my slider
 
 });
-
-
-
-
 
 
 
--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,20 +1,11 @@
 // get html element
 let mute_test = document.getElementById('mute_test');
-let gain_slider = document.getElementById('gainslider');
+var gain_slider = document.getElementById('gainslider');
+let second_slider = document.getElementById('secondslider');
+let third_slider = document.getElementById('thirdslider');
 let label1 = document.getElementById('label1');
 let date = new Date();
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-var gainNodes = [];
-var audioContexts = [];
-var tabs = [];
-=======
->>>>>>> parent of b98dc9f... properly linked my gain node to my slider
-=======
->>>>>>> parent of b98dc9f... properly linked my gain node to my slider
-=======
->>>>>>> parent of b98dc9f... properly linked my gain node to my slider
+var gainNode;
 
 // IF I CAN EVER WORK OUT HOW TO GET THIS BACKGROUND PORT THING TO WORK
 // THE IDEA IS THAT YOU ONLY SAVE THE STATE WHEN THE POPUP ACTUALLY CLOSES, AND I THINK THIS THING ACHIEVES THIS
@@ -26,8 +17,11 @@ backgroundPort.onMessage.addListener(function(message){});
 // add onclick for it
 mute_test.onclick = function(element) {
     console.log("button pressed");
+
+    // request the tab capture - maybe I need to do it in here...?
     chrome.runtime.sendMessage({
         action: 'fetch_audio_stream',
+        slider_value: gain_slider.style.value
     });
 }
 
@@ -46,43 +40,14 @@ window.addEventListener("change", function(event){
 
     // however, we only want to store the value when popup is closed, but do!!
     // want to update audio values upon a slider change.
+    /*
+    chrome.runtime.sendMessage({
+        action: 'update-gain',
+        slider_value: gain_slider.value
+    });
+    */
     
 });
-
-// TODOOOOOOOOOOO
-/*
-make it so that when it loads the page, ot gets access to that objects list
-
-then for each time the popup is loaded, it also does the fetch audio stream thing - BUT ONLY if that tab is not currently
-being tracked, so it needs to get the list of tabs first, and see if it is not there you know - easiest to do that in document.
-
-then it needs to dynamically add sliders!!!! to the popup depending on the number of audio contexts being tracked,
-including the current. 
-
-need to handle for errors so that if the audio stream thing fails, it does nothing - doesn't try
-
-to add something to the list 
-
-then get rid of the button checking
-
-then need to make it so that it only checks for the update if the tab isn't already currently registered - 
-store the tab in a currently stored list or something
-
-then on load need to insert a load of new sliders for each thing that needs to be checked.
-
-then the slider needs to update the gain for that thing okay.
-does that make sense...?
-good good good.
-
-add an event listener for each slider instead
-
-TEST OUT THAT WINDOW EVENT CLOSE THING SEE WHAT HAPPENS
-
-MUST USE chrome.extension.getBackgroundPage().gainNode; this kind of thing to get stuff from the background tab
-but actually due to callbacks I didn't need to do that
-*/
-
-
 
 // input is the message used for sliders that gradually change their value
 window.addEventListener("input", function(event){
@@ -91,98 +56,41 @@ window.addEventListener("input", function(event){
     if(event.target.parentElement.className === "slider"){
         label1.innerHTML = "slider value: " + evvalue;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        // now update the audio gain for the relevant tab
-        gainNodes[parseInt(event.target.id)].gain.value = parseInt(evvalue)/100;
-=======
-=======
->>>>>>> parent of b98dc9f... properly linked my gain node to my slider
-=======
->>>>>>> parent of b98dc9f... properly linked my gain node to my slider
-
         // now update the audio gain in the two different ways with the two different sliders.
-        // version 1, by sending a message:
+        /*
         chrome.runtime.sendMessage({
-            action: 'gain-update',
-            // need to deal with is this a string not a number...?
-            value: evvalue
+            action: 'update-gain',
+            slider_value: evvalue
         });
-
-
-        // version 2 - where we already have the audio thing
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of b98dc9f... properly linked my gain node to my slider
-=======
->>>>>>> parent of b98dc9f... properly linked my gain node to my slider
-=======
->>>>>>> parent of b98dc9f... properly linked my gain node to my slider
+        */
+       gainNode.gain.value = parseInt(evvalue)/100;
     }
 });
 
 /*
     On load, setup the UI
 */
-var sendResp = function(evt){
-    // get parameters
-    audioContexts = chrome.extension.getBackgroundPage().audioContextsList;
-    gainNodes = chrome.extension.getBackgroundPage().gainNodesList;
-    tabs = chrome.extension.getBackgroundPage().tabsList;
-    console.log("audiocontexts, gainnodes, tabs", audioContexts, gainNodes, tabs);
-
-    // inject extra sliders
-    for(i = 0; i < gainNodes.length; i++){
-        let slider = document.createElement('input');
-        
-        slider.id = toString(i);
-        slider.type = "range";
-        slider.min = "0";
-        slider.max = "100";
-        slider.value = "100";
-    }
-    
-}
-
-
 window.addEventListener("load", function(){
     console.log("the popup was loaded on");
     // set the slider value to the value in storage
 
-    // get the slider value- just for keeping that slider up to date
+    // get the slider value
     chrome.storage.sync.get('sliderValue', function(data){
         console.log("setting value to: ", data.sliderValue);
         gain_slider.value = data.sliderValue;
     });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     // now fetch the gain node from the background...
     // so yes, everything is going to be handled from the popup script now oh yes I know, its terrible.
-    // TODO need to replace with fetch the list, hopefully then can just access any nodes in it quite easily.
-    //console.log("background page: ", chrome.extension.getBackgroundPage().gainNode);
-    //gainNode = chrome.extension.getBackgroundPage().gainNode;
-    //console.log("background page2 :", chrome.extension.getBackgroundPage().gainNode);
+    console.log("background page: ", chrome.extension.getBackgroundPage().gainNode);
+    gainNode = chrome.extension.getBackgroundPage().gainNode;
+    console.log("background page2 :", chrome.extension.getBackgroundPage().gainNode);
+    
 
-=======
->>>>>>> parent of b98dc9f... properly linked my gain node to my slider
-=======
->>>>>>> parent of b98dc9f... properly linked my gain node to my slider
-=======
->>>>>>> parent of b98dc9f... properly linked my gain node to my slider
 });
 
-// now to ask for the audio info
-chrome.runtime.sendMessage({
-    action: 'fetch_audio_stream',
-}, sendResp);
-
-
-/// dammnit need to test if this works as well
-window.addEventListener("close", function(event){
-    //chrome.storage.sync.set({'sliderValue': gain_slider.value});
+window.addEventListener("close", function(){
+    chrome.storage.sync.set({'sliderValue': event.target.value});
 });
 
 // should instead out the audio_stream_fetch here if you want it, and then put a button for load
